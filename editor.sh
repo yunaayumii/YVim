092255317152a843631fc3a64aa3119f6dff6a25
Yuan Miguel Rubio <yuanmiguelrubio@Yuans-MacBook-Air.local>
2026-06-26 01:24:51 +0800

    feat: implement logic for different modes, no file saving yet

--- editor.sh
+++ editor.sh
@@ -18,32 +18,37 @@ while true; do
             # Printing a newline so we start typing on a fresh line
             echo ""
             echo "-- INSERT --"
-        else
-            # We will ignore standard normal keys for now unless they are 'q' or 'i'
+
         fi
     else
-        # INSERT MODE LOGIC
+        # INSERT MODE LOGIC                                                                                                                              
         if [[ "$keypress" == $'\e' ]]; then
             MODE="NORMAL"
             echo ""
             echo "-- NORMAL --"
         
         # TODO: Handle Backspace
-        elif [[ "$keypress" == $"\b" ]]; then
-            if [[ "CHANGE_ME_BUFFER_LENGTH_CHECK" ]]; then
+        elif [[ "$keypress" == $'\x7f' ]]; then
+            if [[ ${#BUFFER} -gt 0 ]]; then
                 # 1. Visually erase
+                echo -ne "\b \b"
                 # 2. Logically remove last char from BUFFER
+                BUFFER="${BUFFER%?}"
             fi
             
         # TODO: Handle Enter
-        elif [[ "$keypress" == $"\r" ]]; then
-            # 1. Visually drop to a new line
-            # 2. Reset the BUFFER
+        elif [[ "$keypress" == "" ]]; then
+            # Logically append a newline character to the BUFFER
+            BUFFER="${BUFFER}"$'\n'
+            echo ""
+
             
         # TODO: Handle standard typing
         else
             # 1. Logically append $keypress to BUFFER
+            BUFFER="${BUFFER}${keypress}"
             # 2. Visually print $keypress (no newline)
+            echo -n "$keypress"
         fi
     fi
 done
