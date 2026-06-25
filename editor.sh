6aceb19112b5d951d3e02eb11f153076a5d0fe54
Yuan Miguel S. Rubio <yuanmiguelrubio@gmail.com>
2026-06-25 18:23:49 +0800

    feat: added structure for logic

--- editor.sh
+++ editor.sh
@@ -1,33 +1,49 @@
 #!/bin/bash
 
-# TODO: Initialize the MODE variable to "NORMAL"
 MODE="NORMAL"
+# TODO: Initialize empty BUFFER
+BUFFER=""
 
 echo "Welcome to yvim. Mode: $MODE. (PRESS 'q' in Normal Mode to quit)"
 
 while true; do
     read -rsn1 keypress
-
-    # TODO: Check if MODE is "NORMAL"
     if [[ "$MODE" == "NORMAL" ]]; then
+        # NORMAL MODE LOGIC 
         if [[ "$keypress" == "q" ]]; then 
             echo "Exiting..."
             break
         elif [[ "$keypress" == "i" ]]; then
-            # TODO: Switch mode to INSERT and print a visual indicator
             MODE="INSERT"
-            echo "Switched to Insert Mode"
+            # Printing a newline so we start typing on a fresh line
+            echo ""
+            echo "-- INSERT --"
         else
-            echo "Normal command: $keypress"
+            # We will ignore standard normal keys for now unless they are 'q' or 'i'
         fi
-    # TODO: Handle the "INSERT" mode logic
     else
+        # INSERT MODE LOGIC
         if [[ "$keypress" == $'\e' ]]; then
-            # TODO: Switch mode back to NORMAL and print a visual indicator
             MODE="NORMAL"
-            echo "Switched to Normal Mode" 
+            echo ""
+            echo "-- NORMAL --"
+        
+        # TODO: Handle Backspace
+        elif [[ "$keypress" == $"\b" ]]; then
+            if [[ "CHANGE_ME_BUFFER_LENGTH_CHECK" ]]; then
+                # 1. Visually erase
+                # 2. Logically remove last char from BUFFER
+            fi
+            
+        # TODO: Handle Enter
+        elif [[ "$keypress" == $"\r" ]]; then
+            # 1. Visually drop to a new line
+            # 2. Reset the BUFFER
+            
+        # TODO: Handle standard typing
         else
-            echo "Typing: $keypress"
+            # 1. Logically append $keypress to BUFFER
+            # 2. Visually print $keypress (no newline)
         fi
     fi
 done
